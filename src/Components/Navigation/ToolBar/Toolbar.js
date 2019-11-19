c4110d07df273ad7b3d944ca36e1aae3121fd9e6
Sneha Thadani <snehacopper@gmail.com>
2019-11-19 13:29:47 -0800

    HamBurger Added

--- src/Components/Navigation/ToolBar/Toolbar.js
+++ src/Components/Navigation/ToolBar/Toolbar.js
@@ -2,12 +2,13 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Hamburger from '../../Navigation/NavigationItems/Hamburger';
 
 
 const Toolbar = (props)=> (
     <header className={classes.ToolBar}>
-        <div> MENU </div>
-        <Logo/>
+        <Hamburger/>
+        <Logo height="80%"/>
         <nav>
            <NavigationItems/>
         </nav>
